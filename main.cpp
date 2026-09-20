ea270e86f201261cb8d7fc65a4c743e3a94cd37a
Marco <mazapata053@gmail.com>
2026-09-20 16:42:36 -0700

    feat: define global constants and created Racer struct.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+const int NR_RACERS = 3;
+const int NR_LAPS = 3;
+
+struct Racer {
+    int raceNumber;
+    int placement;
+    double * lapTimes = nullptr;  // Inner dynamic array to track lap times
+
+    // Destructor to auto clean memory
+    ~Racer() {
+        if (lapTimes)
+            delete [] lapTimes;
+        lapTimes = nullptr;
+    }
+};
+
 
 int main() {
 
